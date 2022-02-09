1250fe58c203e932eade66a1193f4c16fd6f693d
crespire <li.simmon@gmail.com>
2022-02-09 15:52:35 -0500

    update app to allow editing users. Added index to redirect to

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -6,8 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if user.save
-      redirect_to new_user_path
+    if @user.save
+      redirect_to root_path
     else
       render :new
     end
@@ -15,6 +19,16 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
